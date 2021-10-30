5423457d66625f81f41b2911743595bb545dc2cb
Beeraka Krupa Kiranmai <krupakiranmai1122@gmail.com>
2021-10-30 13:07:06 +0530

    Added this for demonstartion purpose will delete later

---
+++ databasedemoapp.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import yaml
+
+app = Flask(__name__)
+# Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def users():
+    cur = mysql.connection.cursor()
+    # return "Success"
+    resultValue = cur.execute("SELECT * FROM user")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        cur.close()
+        return render_template('databaseconnectiondemo.html',userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
