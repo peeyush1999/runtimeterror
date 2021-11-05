efed4b1ec1fcf90ae278341e725621793cd9d74a
peeyush1999 <sahupeeyushsahu@gmail.com>
2021-11-05 11:44:03 +0530

    sadmin

--- app.py
+++ app.py
@@ -18,6 +18,114 @@ app.config['MYSQL_DB'] = 'ssdproject'
 mysql = MySQL(app)
 
 
+
+#================Admin Part=======================================
+@app.route('/admin', methods=['GET'])
+def admin():
+    
+    return render_template('admin.html')
+
+
+@app.route('/login', methods=['POST'])
+def userdetails():
+    username = request.form.get('Email')
+    conn = mysql.connection
+    cur = conn.cursor()
+    userid = cur.execute("SELECT user.userid,user.name  FROM user where user.username = %s",[username])
+    userDetails = cur.fetchall()
+    cur.execute("INSERT INTO `registration` (`workshopid`, `userid`) VALUES (%s , %s)",[0 , userDetails[0][0]])
+    cur.close()
+    conn.commit()
+    return render_template('databaseconnectiondemo.html')
+
+
+@app.route('/people', methods=['post'])
+def userdeets():
+    conn = mysql.connection
+    cur = conn.cursor()
+    d={}
+    count = cur.execute("SELECT user.name FROM user where user.type = 'participant' AND user.userid IN (SELECT reg.userid FROM registration reg)")
+    print(count)
+    if(count > 0):
+        userDetails = cur.fetchall()
+        print(userDetails)
+        cur.close()
+        conn.commit()
+        d = {}
+        i=0
+        for x in userDetails:
+            d[i] = x[0]
+            i = i+1
+    return jsonify(d)
+
+
+
+def arrangeRandom(userIds, count):
+    r = random.sample(range(len(userIds)), len(userIds))
+    print(r)
+    users = []
+    for i in userIds:
+        users.append(i[0])
+    i = 0
+    c = 0
+    d = {}
+    v = []
+    j = 0
+    for id in range(len(users) + 1):
+        if (j == len(users)):
+            d[i] = v
+            break
+        if (c == count):
+            d[i] = v
+            i = i + 1
+            c = 0
+            v = []
+        v.append(users[r[j]])
+        c = c + 1
+        j = j + 1
+    print(d)
+    return d
+
+
+@app.route('/create', methods=["POST"])
+def createTeams():
+    conn = mysql.connection
+    cur = conn.cursor()
+    resultValue = cur.execute("SELECT * FROM user")
+    userDetails = cur.fetchall()
+    resultValu = cur.execute(
+        "SELECT reg.userid FROM registration reg WHERE reg.userid IN (SELECT user.userid FROM user WHERE type = 'participant')"
+    )
+    if resultValue > 0:
+        l = {}
+        userIds = cur.fetchall()
+        d = arrangeRandom(userIds, 2)
+        print(userIds)
+        # for i in d.keys():
+        #     id = i
+        #     for j in d[i]:
+        #         cur.execute("INSERT INTO `group` (`workshopid`, `userid`, `groupid`) VALUES (%s, %s, %s)",(1,j,id))
+        #         conn.commit()
+        u = {}
+        k = 0
+        for x in d.keys():
+            u[x] = []
+            for y in d[x]:
+                res = cur.execute(
+                    "SELECT user.name FROM user WHERE user.userid = %s", [y])
+                if (res > 0):
+                    val = cur.fetchall()
+                    u[x].append(val)
+        cur.close()
+        conn.commit()
+        return jsonify(u)
+
+
+#=================================================================
+
+
+
+
 @app.route("/updatePhase",methods=['GET'])
 def updatephase():
 	phase = request.args.get('curphase')
