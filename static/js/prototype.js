b218b91260f69c6267c02518e2240d8b8223c6fa
GaganAgarwal1813 <48082205+GaganAgarwal1813@users.noreply.github.com>
2021-11-13 16:02:54 +0530

    Changed some front end and added a back end functionality

--- static/js/prototype.js
+++ static/js/prototype.js
@@ -184,109 +184,6 @@ function select(id)
 
 
 
-
-
-/*window.onload = function() {
-	var myCanvas = document.getElementById("myCanvas1");
-	var ctx = myCanvas.getContext("2d");
-    
-    // Fill Window Width and Height
-    myCanvas.width = 300;
-	myCanvas.height = 300;
-	
-	// Set Background Color
-    ctx.fillStyle="#fff";
-    ctx.fillRect(0,0,myCanvas.width,myCanvas.height);
-	
-    // Mouse Event Handlers
-	if(myCanvas){
-		var isDown = false;
-		var canvasX, canvasY;
-		ctx.lineWidth = 5;
-		
-		$(myCanvas)
-		.mousedown(function(e){
-			isDown = true;
-			ctx.beginPath();
-			canvasX = e.pageX - myCanvas.offsetLeft;
-			canvasY = e.pageY - myCanvas.offsetTop;
-			ctx.moveTo(canvasX, canvasY);
-		})
-		.mousemove(function(e){
-			if(isDown !== false) {
-				canvasX = e.pageX - myCanvas.offsetLeft;
-				canvasY = e.pageY - myCanvas.offsetTop;
-				ctx.lineTo(canvasX, canvasY);
-				ctx.strokeStyle = "#000";
-				ctx.stroke();
-			}
-		})
-		.mouseup(function(e){
-			isDown = false;
-			ctx.closePath();
-		});
-	}
-	
-	// Touch Events Handlers
-	draw = {
-		started: false,
-		start: function(evt) {
-
-			ctx.beginPath();
-			ctx.moveTo(
-				evt.touches[0].pageX,
-				evt.touches[0].pageY
-			);
-
-			this.started = true;
-
-		},
-		move: function(evt) {
-
-			if (this.started) {
-				ctx.lineTo(
-					evt.touches[0].pageX,
-					evt.touches[0].pageY
-				);
-
-				ctx.strokeStyle = "#000";
-				ctx.lineWidth = 5;
-				ctx.stroke();
-			}
-
-		},
-		end: function(evt) {
-			this.started = false;
-		}
-	};
-	
-	// Touch Events
-	myCanvas.addEventListener('touchstart', draw.start, false);
-	myCanvas.addEventListener('touchend', draw.end, false);
-	myCanvas.addEventListener('touchmove', draw.move, false);
-	
-	// Disable Page Move
-	document.body.addEventListener('touchmove',function(evt){
-		evt.preventDefault();
-	},false);
-};
-
-*/
-
-
-
-
-// function createSticky(id){
-    
-// 	$('#stkyNts').append('<li class="box">'+'<div><canvas id="myCanvas'+id+'">s</canvas></div><button onclick="select('+id+');">use</button></li>');
-// }
-
-
-
-
-
-
-
 function createSticky(id){
 
 	console.log('called with id',id);
@@ -309,52 +206,10 @@ function createSticky(id){
 //'<li class="box">'+'<div><canvas id="myCanvas'+id+'">s</canvas></div><button onclick="select('+id+');">use</button></li>'
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // BACKGROUND
 
-
-
 const bg = new MousemoveBGColors();
-/* const bg = new MousemoveBGColors({
-       target : 'body' //(string) element selector // default : 'body'
-      ,transition : .2 //(float|int) transition duration // default: .09
-      ,delay:.03 //(float|int) transition delay // default: 0 
-      ,center: [255, 255, 255] //(array) rgb color in the center // default : [168, 218, 218]
-      ,topMiddle: [168, 218, 218]  //(array) rgb color in top middle // default : [168, 218, 218]
-      ,leftMiddle: [236, 236, 236]  //(array) rgb color in the center // default : [168, 218, 218]
-      ,rightMiddle: [255, 120, 63] //(array) rgb color in the center // default : [255, 128, 63]
-      ,bottomMiddle: [0, 107, 107] //(array) rgb color in the center // default : [168, 218, 218]
-      });
-*/
+
 const initButtons = (function(){
     const btn= document.getElementById('destroyInit');
           btn.addEventListener('click',(e) => { 
